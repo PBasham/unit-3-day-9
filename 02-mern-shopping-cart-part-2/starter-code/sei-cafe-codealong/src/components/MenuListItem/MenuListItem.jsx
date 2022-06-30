7ee517660acbad74cf31258fe24a1a8e3c23e32d
patrickbasham <PatrickTBasham@gmail.com>
2022-06-30 13:15:07 -0400

    updated handleAddToOrder code to start adding item to cart

--- 02-mern-shopping-cart-part-2/starter-code/sei-cafe-codealong/src/components/MenuListItem/MenuListItem.jsx
+++ 02-mern-shopping-cart-part-2/starter-code/sei-cafe-codealong/src/components/MenuListItem/MenuListItem.jsx
@@ -7,7 +7,7 @@ export default function MenuListItem({ menuItem, handleAddToOrder }) {
       <div className="name">{menuItem.name}</div>
       <div className="buy">
         <span>${menuItem.price.toFixed(2)}</span>
-        <button className="btn-sm" onClick={event => handleAddToOrder(menuItem._id)}>
+        <button className="btn-sm" onClick={() => handleAddToOrder(menuItem._id)}>
           ADD
         </button>
       </div>
--- 02-mern-shopping-cart-part-2/starter-code/sei-cafe-codealong/src/pages/NewOrderPage/NewOrderPage.jsx
+++ 02-mern-shopping-cart-part-2/starter-code/sei-cafe-codealong/src/pages/NewOrderPage/NewOrderPage.jsx
@@ -47,9 +47,13 @@ export default function NewOrderPage({ user, setUser }) {
           Event Handlers
   ========================================*/
   const handleAddToOrder = async (itemId) => {
-    // Baby step
-    alert(`add item: ${itemId}`)
+    // alert(`add item: ${itemId}`)
+    // 1. Call the addItemToCart function in ordersAPI, passing to it the itemId, and assign the resolved promise to a variable named cart.
+    const updatedCart = await ordersAPI.addItemToCart(itemId)
+  // 2. Update the cart state with the updated cart received from the server
   }
+
+  /*========== End Event Handlers ==========*/
 
   return (
     <main className="NewOrderPage">
